92a8cb5b92d95ebdac14eccbff6f46e9303a69a8
Henrique Roma <roma.fulldev@gmail.com>
2021-05-06 18:30:46 -0300

    Criação do primeiro componente

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'react-native';
+
+function Tela() {
+    return <Text>Primeiro Componente</Text>
+}
+
+export default Tela
