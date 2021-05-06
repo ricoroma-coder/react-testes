d30a538f5ad5b67e4f4e0f9323e2f0fab02d4d18
Henrique Roma <roma.fulldev@gmail.com>
2021-05-06 18:40:39 -0300

    Estudando as diversas maneiras de declarar e exportar componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text } from 'react-native'
 
-function Tela() {
+//diferentes maneiras de declarar funções
+//function Tela() {
+//    return <Text>Primeiro Componente</Text>
+//}
+const Tela = function () {
     return <Text>Primeiro Componente</Text>
 }
 
 export default Tela
+//exportando a função direto
+//export default function () {
+//    return <Text>Primeiro Componente</Text>
+//}
+//arrow function
+//export default () => {
+//    return <Text>Primeiro Componente</Text>
+//}
+//simplificada
+//export default () => <Text>Primeiro Componente</Text>
