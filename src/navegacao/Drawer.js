35195fdf94e6b4cae1c57bb2f3bb3ba8eb047599
Henrique Roma <roma.fulldev@gmail.com>
2021-06-09 19:00:41 -0300

    Inserindo botão para abrir o drawer

--- src/navegacao/Drawer.js
+++ src/navegacao/Drawer.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import Tela1 from '../componentes/Tela1'
 import Tela2 from '../componentes/Tela2'
 import Tela3 from '../componentes/Tela3'
+import Tela4 from '../componentes/Tela4'
 
 const Drawer = createDrawerNavigator()
 
@@ -11,5 +12,6 @@ export default props => (
         <Drawer.Screen name="Tela1" component={Tela1} />
         <Drawer.Screen name="Tela2" component={Tela2} />
         <Drawer.Screen name="Tela3" component={Tela3} />
+        <Drawer.Screen name="Tela4" component={Tela4} />
     </Drawer.Navigator>
 )
